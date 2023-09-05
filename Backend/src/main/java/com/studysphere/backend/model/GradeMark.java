27a63c14126d9dea5a772e9cd733ef19ffbb5e1f
Silvia Lupsa <lupsasilvia@gmail.com>
2023-09-05 15:53:14 +0300

    added grade mark enum

---
+++ Backend/src/main/java/com/studysphere/backend/model/GradeMark.java
@@ -0,0 +1,29 @@
+package com.studysphere.backend.model;
+
+public enum GradeMark {
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10);
+    private final int value;
+
+    GradeMark(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+}
