098328fc0f776e6796c9a40c86599ff0e84ebfbf
Your Name <sinziana.babu@yahoo.com>
2023-09-05 16:04:33 +0300

    Person class creates

---
+++ Backend/src/main/java/com/studysphere/backend/model/Person.java
@@ -0,0 +1,18 @@
+package com.studysphere.backend.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Person {
+    private Long id;
+    private String name;
+    private LocalDate birthdate;
+    private List<String> messages;
+
+    public Person(Long id, String name, LocalDate birthdate, List<String> messages) {
+        this.id = id;
+        this.name = name;
+        this.birthdate = birthdate;
+        this.messages = messages;
+    }
+}
