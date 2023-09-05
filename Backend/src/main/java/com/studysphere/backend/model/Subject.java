d67dfd715343a19e9298ca6cf98bc15d3a8f320e
Silvia Lupsa <lupsasilvia@gmail.com>
2023-09-05 15:49:02 +0300

    added subject enum

---
+++ Backend/src/main/java/com/studysphere/backend/model/Subject.java
@@ -0,0 +1,22 @@
+package com.studysphere.backend.model;
+
+public enum Subject {
+    ROMANIAN,
+    MATHEMATICS,
+    GEOGRAPHY,
+    HISTORY,
+    ENGLISH,
+    BIOLOGY,
+    OPTIONAL,
+    DRAWING,
+    TECHNOLOGICAL_EDUCATION,
+    PHYSICS,
+    GERMAN,
+    CHEMISTRY,
+    LATIN,
+    FRENCH,
+    SPORTS,
+    RELIGION,
+    CIVIC_EDUCATION,
+    MUSIC
+}
