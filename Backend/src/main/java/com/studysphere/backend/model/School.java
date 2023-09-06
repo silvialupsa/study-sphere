acfdeb7a3897998ee045d137d128acfc01a39e7b
Your Name <sinziana.babu@yahoo.com>
2023-09-06 15:22:56 +0300

    Add attendance, some relationships between classes

--- Backend/src/main/java/com/studysphere/backend/model/School.java
+++ Backend/src/main/java/com/studysphere/backend/model/School.java
@@ -1,63 +1,34 @@
 package com.studysphere.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.studysphere.backend.model.types.Facilities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Entity
+@Data
+@Table(name = "schools")
+@AllArgsConstructor
+@NoArgsConstructor
 public class School {
-    private final Long id;
-    private final String name;
-    private Professor schoolPrincipal;
-    private List<Grade> gradeList;
-    private List<Student> studentList;
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "inspectorates.id", nullable = false)
+    private SchoolInspectorate schoolInspectorate;
+
+//    private Professor schoolPrincipal;
+//    private List<Grade> gradeList;
+//    private List<Student> studentList;
     private List<Facilities> facilitiesList;
 
-    public School(Long id, String name, Professor schoolPrincipal, List<Grade> gradeList, List<Facilities> facilitiesList) {
-        this.id = id;
-        this.name = name;
-        this.schoolPrincipal = schoolPrincipal;
-        this.gradeList = gradeList;
-        this.studentList = gradeList.stream().flatMap(grade -> grade.getStudentList().stream()).toList();
-        this.facilitiesList = facilitiesList;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Professor getSchoolPrincipal() {
-        return schoolPrincipal;
-    }
-
-    public void setSchoolPrincipal(Professor schoolPrincipal) {
-        this.schoolPrincipal = schoolPrincipal;
-    }
-
-    public List<Grade> getGradeList() {
-        return gradeList;
-    }
-
-    public void setGradeList(List<Grade> gradeList) {
-        this.gradeList = gradeList;
-    }
-
-    public List<Student> getStudentList() {
-        return studentList;
-    }
-
-    public void setStudentList(List<Student> studentList) {
-        this.studentList = studentList;
-    }
-
-    public List<Facilities> getFacilitiesList() {
-        return facilitiesList;
-    }
-
-    public void setFacilitiesList(List<Facilities> facilitiesList) {
-        this.facilitiesList = facilitiesList;
-    }
 }
