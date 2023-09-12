5de5bf091700b7e8ef3e2eb0247200c7947d17e0
Silvia Lupsa <lupsasilvia@gmail.com>
2023-09-12 16:35:26 +0300

    create carousel

--- frontend/src/Pages/WelcomePage.js
+++ frontend/src/Pages/WelcomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Carousel from "../components/Carousel";
 
 const WelcomePage = () => {
     return (
@@ -9,6 +10,7 @@ const WelcomePage = () => {
                     <h1>Welcome to Our School</h1>
                     <p>This is the welcome page of our school website.</p>
                     <Link to="/students" className="btn btn-primary">Go to Student List</Link>
+                    <Carousel/>
                 </div>
             </div>
         </div>
