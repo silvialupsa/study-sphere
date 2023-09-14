53c7b030f5f55d97ba111ed752242275bb5f21a9
Your Name <sinziana.babu@yahoo.com>
2023-09-14 10:09:49 +0300

    start on creating forms

---
+++ frontend/src/components/StudentCreator.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import StudentForm from "./StudentForm";
+import StudentList from "../Pages/StudentList";
+
+const createStudent = (student)=> {
+    return fetch("/students/",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application.json",
+        },
+        body:JSON.stringify(student),
+    }).then((res) => res.json());
+}
+
+const StudentCreator = () => {
+    const navigate = useNavigate();
+
+    const handleCreateStudent = (student) => {
+        createStudent(student).then(()=> {
+            navigate("/students")
+        })
+    }
+
+    return(
+        <StudentForm
+            onCancel={()=> navigate("/students")}
+            onSave={handleCreateStudent}/>
+    )
+}
+
+export default StudentCreator
