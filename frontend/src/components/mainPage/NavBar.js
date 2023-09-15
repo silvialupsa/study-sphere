377c0ef110e8158593523a97b875768af15b24d0
Silvia Lupsa <lupsasilvia@gmail.com>
2023-09-15 10:59:18 +0300

    edited welcome page

--- frontend/src/components/mainPage/NavBar.js
+++ frontend/src/components/mainPage/NavBar.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import Button from 'react-bootstrap/Button';
-import { NavDropdown } from "react-bootstrap";
+import {NavDropdown} from "react-bootstrap";
 import logo from '../../images/white-no-bg.png';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 const NavBar = () => {
-    return (
+    return (<div>
         <nav className={`navbar navbar-expand-lg navbar-light nav-underline`}>
-            <img id="logo" src={logo} width={80} alt="logo" />
+            <img id="logo" src={logo} width={80} alt="logo"/>
             <div className="container justify-content-end">
                 <a className="nav-link mx-3" href="/">
                     Home
@@ -16,13 +16,13 @@ const NavBar = () => {
                 <a className="nav-link mx-3" href="#">
                     About Us
                 </a>
-                <NavDropdown title="Features" id="basic-nav-dropdown" >
+                <NavDropdown title="Features" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">
                         Another action
                     </NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
+                    <NavDropdown.Divider/>
                     <NavDropdown.Item href="#action/3.4">
                         Separated link
                     </NavDropdown.Item>
@@ -44,16 +44,64 @@ const NavBar = () => {
                 {/*    </div>*/}
                 {/*</form>*/}
 
-
                 <Button className="log-in mx-3">
                     Log in
                 </Button>
+
                 <Button className="get-started mx-3">
                     Get started
                 </Button>
             </div>
         </nav>
-    );
+
+        {/*<nav className="navbar navbar-expand-lg bg-body-tertiary nav-underline">*/}
+        {/*    <div className="container-fluid">*/}
+        {/*        <img id="logo" src={logo} width={80} alt="logo"/>*/}
+        {/*        <div className="collapse navbar-collapse justify-content-end" id="navbarText">*/}
+        {/*            <ul className="navbar-nav me-auto mb-2 mb-lg-0">*/}
+        {/*                <li className="nav-item">*/}
+        {/*                    <a className="nav-link mx-3" href="/">*/}
+        {/*                        Home*/}
+        {/*                    </a>*/}
+        {/*                </li>*/}
+        {/*                <li className="nav-item">*/}
+        {/*                    <a className="nav-link mx-3" href="#">*/}
+        {/*                        About Us*/}
+        {/*                    </a>*/}
+        {/*                </li>*/}
+        {/*                <li>*/}
+        {/*                    <NavDropdown title="Features" id="basic-nav-dropdown">*/}
+        {/*                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
+        {/*                        <NavDropdown.Item href="#action/3.2">*/}
+        {/*                            Another action*/}
+        {/*                        </NavDropdown.Item>*/}
+        {/*                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>*/}
+        {/*                        <NavDropdown.Divider/>*/}
+        {/*                        <NavDropdown.Item href="#action/3.4">*/}
+        {/*                            Separated link*/}
+        {/*                        </NavDropdown.Item>*/}
+        {/*                    </NavDropdown>*/}
+        {/*                </li>*/}
+        {/*                <li className="nav-item">*/}
+        {/*                    <a className="nav-link mx-3" href="#">*/}
+        {/*                        Plans*/}
+        {/*                    </a>*/}
+        {/*                </li>*/}
+        {/*                <li className="nav-item">*/}
+        {/*                    <Button className="log-in mx-3">*/}
+        {/*                        Log in*/}
+        {/*                    </Button>*/}
+        {/*                </li>*/}
+        {/*                <li className="nav-item">*/}
+        {/*                    <Button className="get-started mx-3">*/}
+        {/*                        Get started*/}
+        {/*                    </Button>*/}
+        {/*                </li>*/}
+        {/*            </ul>*/}
+        {/*        </div>*/}
+        {/*    </div>*/}
+        {/*</nav>*/}
+    </div>);
 };
 
 export default NavBar;
--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -3,6 +3,7 @@ import ImageCarousel from "../components/mainPage/ImageCarousel";
 import PartnersCarousel from "../components/mainPage/PartnersCarousel";
 import splash from "../images/bg.png"
 import videoBg from "../videos/video.mp4"
+import Button from "react-bootstrap/Button";
 
 const WelcomePage = () => {
     return (
@@ -10,16 +11,18 @@ const WelcomePage = () => {
             <div className="video-container">
                 <video id="backgroundVideo" className="card-img" src={videoBg} autoPlay loop muted/>
                 <div className="card-img-overlay m-auto position-absolute top-50 start-0">
-                    {/*<h1 className="card-title fw-bold mb-3">Every Service Is NOW More Accessible Than Ever</h1>*/}
-                    {/*<a className="btn btn-primary fw-bold p-3">POST Your Ads NOW FOR FREE</a>*/}
+                    <h1 className="card-title fw-bold mb-3 welcome-text">Your Integrated School Platform</h1>
+                    <div className="d-flex justify-content-center">
+                        <Button className="get-button get-started mx-3">
+                            Get started
+                        </Button>
+                    </div>
                 </div>
             </div>
             <div className="content-container">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-6 offset-md-3 text-center welcome-container">
-                            <h1>Welcome to Our School</h1>
-                            <p>This is the welcome page of our school website.</p>
                             <ImageCarousel/>
                             <PartnersCarousel/>
                         </div>
