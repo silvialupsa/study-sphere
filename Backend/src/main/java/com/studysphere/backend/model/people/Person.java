584c7977b8955f327d1927529621b117fb7d1a2f
Your Name <sinziana.babu@yahoo.com>
2023-09-18 12:37:46 +0300

    change data for inspectorates

--- Backend/src/main/java/com/studysphere/backend/model/people/Person.java
+++ Backend/src/main/java/com/studysphere/backend/model/people/Person.java
@@ -19,6 +19,7 @@ public class Person {
     @Id
     @GeneratedValue
     private Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private LocalDate birthdate;
 }
