a5d5831bfc140555da09928dad39c214b533d906
Silvia Lupsa <lupsasilvia@gmail.com>
2023-09-19 09:24:41 +0300

    nab var

--- Backend/src/main/java/com/studysphere/backend/repository/SchoolRepository.java
+++ Backend/src/main/java/com/studysphere/backend/repository/SchoolRepository.java
@@ -1,7 +1,10 @@
 package com.studysphere.backend.repository;
 
 import com.studysphere.backend.model.School;
+import com.studysphere.backend.model.SchoolInspectorate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
 
 public interface SchoolRepository extends JpaRepository<School, Long> {
+    List<School> findSchoolsBySchoolInspectorate(SchoolInspectorate inspectorate);
 }
