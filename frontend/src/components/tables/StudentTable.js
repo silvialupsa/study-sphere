d2cc8e699d919fb4ffc733527bd3ea693257a138
Your Name <sinziana.babu@yahoo.com>
2023-11-15 15:12:00 +0200

    improved attendance management

--- frontend/src/components/tables/StudentTable.js
+++ frontend/src/components/tables/StudentTable.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
+import 'react-calendar/dist/Calendar.css'; // Add this line for default styling
+
 
 const fetchAttendanceByStudentIdAndDate = (studentId, date) => {
     return fetch(`/attendance/student/${studentId}/date/${date}`)
@@ -25,9 +27,9 @@ const fetchAttendanceByStudentIdAndDate = (studentId, date) => {
         });
 };
 
-const updateAttendance = async (studentId, date, attendanceStatus) => {
+const updateAttendance = async (studentId, currentDate, attendanceStatus) => {
     const attendance = {
-        date: date,
+        date: currentDate,
         attendanceStatus: attendanceStatus,
         student: {
             id: studentId,
@@ -35,7 +37,7 @@ const updateAttendance = async (studentId, date, attendanceStatus) => {
     };
     console.log('Request Body:', JSON.stringify(attendance));
 
-    const res = await fetch(`/attendance/update/student/${studentId}/date/${date}`, {
+    const res = await fetch(`/attendance/update/student/${studentId}/date/${currentDate}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
@@ -50,27 +52,26 @@ const updateAttendance = async (studentId, date, attendanceStatus) => {
 const StudentTable = ({students, onDelete, date}) => {
     const [attendanceStatus, setAttendanceStatus] = useState({});
     const [attendanceOn, setAttendanceOn] = useState(false);
-
-
+    const [specificDate, setSpecificDate] = useState(date);
     useEffect(() => {
         const initialStatus = {};
 
         const fetchAttendance = async () => {
             const promises = students.map(async (student) => {
                 try {
-                    const attendance = await fetchAttendanceByStudentIdAndDate(student.id, date);
+                    const attendance = await fetchAttendanceByStudentIdAndDate(student.id, specificDate);
                     if (attendance) {
                         initialStatus[student.id] = attendance.attendanceStatus;
                     } else {
                         initialStatus[student.id] = "PENDING";
                         const defaultAttendance = {
-                            date: date,
+                            date: specificDate,
                             attendanceStatus: "PENDING",
                             student: {
                                 id: student.id,
                             },
                         };
-                        await updateAttendance(student.id, date, defaultAttendance);
+                        await updateAttendance(student.id, specificDate, defaultAttendance);
                     }
                 } catch (error) {
                     console.error('Error fetching attendance:', error);
@@ -84,7 +85,7 @@ const StudentTable = ({students, onDelete, date}) => {
             fetchAttendance();
         }
 
-    }, [students, date, attendanceOn]);
+    }, [students, specificDate, attendanceOn]);
 
     const handleUpdateAttendance = (studentId, attendanceStatus) => {
         updateAttendance(studentId, date, attendanceStatus).then(() => {
@@ -105,6 +106,12 @@ const StudentTable = ({students, onDelete, date}) => {
         <div className="Student-table">
             <h2>Student List</h2>
             <h2>Current date: {date}</h2>
+            <h2>Selected date: {specificDate}</h2>
+
+            <div>
+                <label>Select a different date:  </label>
+                <input type="date" value={specificDate} onChange={(e) => setSpecificDate(e.target.value)}/>
+            </div>
             <button className='btn-group' onClick={() => handleUpdateAttendanceClick()}>Update Attendance</button>
 
             <div className="row">
